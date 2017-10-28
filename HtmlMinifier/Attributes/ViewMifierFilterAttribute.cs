c19849224344f4fc152dc12d193b6ff0e80a78a9
Eidivandi <Eidivandi@live.com>
2017-10-28 13:21:27 +0200

    Ajoutez des fichiers projet.

---
+++ HtmlMinifier/Attributes/ViewMifierFilterAttribute.cs
@@ -0,0 +1,36 @@
+﻿using HtmlMinifier.Filters;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+
+namespace HtmlMinifier.Attributes
+{
+        public class ViewMifierFilterAttribute : ActionFilterAttribute
+        {
+
+            public override void OnActionExecuting(ActionExecutingContext filterContext)
+            {
+
+                var request = filterContext.HttpContext.Request;
+                var response = filterContext.HttpContext.Response;
+
+                response.Filter = new ViewMinifierFilter(response.Filter, s =>
+                {
+                    s = Regex.Replace(s, @"\s+", " ");
+                    s = Regex.Replace(s, @"\s*\n\s*", "\n");
+                    s = Regex.Replace(s, @"\s*\>\s*\<\s*", "><");
+                    s = Regex.Replace(s, @"<!--(.*?)-->", "");   //Remove comments
+
+                    // single-line doctype must be preserved 
+                    var firstEndBracketPosition = s.IndexOf(">");
+                    if (firstEndBracketPosition >= 0)
+                    {
+                        s = s.Remove(firstEndBracketPosition, 1);
+                        s = s.Insert(firstEndBracketPosition, ">");
+                    }
+                    return s;
+                });
+
+            }
+
+        }
+    }
